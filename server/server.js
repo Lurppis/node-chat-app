81613fdaf6d4122df5664b1be387029044ff1bed
Marcin Pilarczyk <marcinpilarczyk@me.com>
2018-02-10 09:42:48 +0100

    Add new message and test, add style

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -17,10 +17,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-	socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback();
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('User', coords.latitude, coords.longitude));
 	});
 });
 
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var { generateMessage } = require('./message.js');
+var { generateMessage, generateLocationMessage } = require('./message.js');
 
 describe('Function generate message', () => {
 	it('Should generate the correct message object', () => {
@@ -10,5 +10,19 @@ describe('Function generate message', () => {
 
 		expect(message.createdAt).toBeA('number');
 		expect(message).toInclude({from, text});
+	});
+});
+
+describe('Function generate location message', () => {
+	it('Should generate the correct location message object', () => {
+		const from = 'Adam';
+		const latitude = 15;
+		const longitude = 20;
+		var message = generateLocationMessage(from, latitude, longitude);
+
+		var expectedUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+		expect(message.createdAt).toBeA('number');
+		expect(message.url).toContain(expectedUrl);
 	});
 });
